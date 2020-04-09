4c2b51be32a592892bddadf1b5e7123aec98e042
Pedro Bini <dev.pedrobini@gmail.com>
2020-04-09 14:54:53 -0300

    Export permissionStatus enum

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,8 @@
+import permissionStatusEnum from './permissionStatus';
 import OneSignal from './oneSignal';
+
+export {
+  permissionStatusEnum as permissionStatus,
+};
 
 export default OneSignal;
---
+++ package/src/permissionStatus.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+enum permissionStatusEnum {
+  Default = 'default',
+  Granted = 'granted',
+  Denied = 'denied',
+}
+
+export default permissionStatusEnum;
