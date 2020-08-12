d7fe8f2db2b2603ea9ca8e17e41bd1245646dea5
Matiyeu <matthieu.crtd@gmail.com>
2020-08-12 17:03:34 +0200

    feat: add isPushNotificationsEnabled() & isPushNotificationsSupported()

--- package/src/oneSignal.types.ts
+++ package/src/oneSignal.types.ts
@@ -2,6 +2,8 @@ export interface IOneSignal {
   notificationPermission: string[];
   registerForPushNotifications: () => Promise<any>;
   getNotificationPermission: () => Promise<string>;
+  isPushNotificationsEnabled: () => Promise<boolean>;
+  isPushNotificationsSupported: () => boolean;
   setEmail: (email: string) => Promise<string>;
   getEmailId: () => Promise<string>;
   getUserId: () => Promise<string>;
