320349c008af32931a8c7d5fd95276e707a5bd99
OneSignal <noreply@onesignal.com>
2023-03-13 19:26:42 +0000

    Update react-onesignal

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <App/>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
