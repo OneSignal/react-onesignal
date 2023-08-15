a7679a27a8323c239c3fe5eea5ef924806e05a1c
OneSignal <noreply@onesignal.com>
2023-08-15 23:46:38 +0000

    Update react-onesignal

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import runOneSignal from './onesignal';
-import { useEffect } from 'react';
+import OneSignal from 'react-onesignal';
 
 function App() {
-  useEffect(() => {
-    runOneSignal();
-  })
+  try {
+    OneSignal.init({
+      appId: "f5d104e9-7462-4bf7-adc8-4ca501cb55b2",
+    }).then(() => {
+      OneSignal.Debug.setLogLevel('trace');
+      OneSignal.login("rodrigo");
+      OneSignal.User.addAlias("myAlias", "1");
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
